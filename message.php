0f81b4a67f3fea426b235cc48d0375318b0e2ca1
voytekh <avoiteh@gmail.com>
2017-09-07 18:36:46 +0400

    1st push

---
+++ message.php
@@ -0,0 +1,20 @@
+<?php
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<messages>
+	<message>
+		<text>текст1</text>
+		<sender>имя1</sender>
+		<date>дата1</date> 
+	</message>
+	<message>
+		<text>текст2</text>
+		<sender>имя2</sender>
+		<date>дата2</date> 
+	</message>
+	<message>
+		<text>текст3</text>
+		<sender>имя3</sender>
+		<date>дата3</date> 
+	</message>
+</messages>';
+?>
